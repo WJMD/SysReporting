40b03f997961663e35d82769960fa5ba396291fe
WANDEXX <55203271+WJMD@users.noreply.github.com>
2023-09-25 21:52:52 -0400

    0.0.01 -  Inicio del proyecto
    - Creación de las carpetas para el Frontend y el Backend.

---
+++ Back/Sysreporting/src/main/java/com/sys/reporting/SysreportingApplication.java
@@ -0,0 +1,13 @@
+package com.sys.reporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysreportingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysreportingApplication.class, args);
+	}
+
+}
---
+++ Back/Sysreporting/src/test/java/com/sys/reporting/SysreportingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sys.reporting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysreportingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
